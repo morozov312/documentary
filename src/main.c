b6cee1c0998ec36c75a0e909c03bec16e1a864e3
moskalt <thetima2@gmail.com>
2020-04-16 23:16:55 +0700

    Added memory free cicle

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@ int main()
 {
     char path[] = "./path/path.txt";
     const int max_len_inp_str = 1000;
-    const int max_quan_str = 100;
+    const int max_quan_str = 10000;
     char** paths = (char**)malloc(max_quan_str * sizeof(char*));
     for (int i = 0; i < max_quan_str; i++) {
         paths[i] = (char*)malloc(max_len_inp_str * sizeof(char));
@@ -18,5 +18,9 @@ int main()
     }
     paths = path_reading(path, paths);
     document_handle(paths);
+    for (int i = 0; i < max_quan_str; i++) {
+        free(paths[i]);
+    }
+    free(paths);
     return 0;
 }
