fa8ce596479dd9e78acd11906ff198565f043d92
moskalt <thetima2@gmail.com>
2020-04-17 23:50:53 +0700

    Added docs_gen function(version 1.0)

--- src/documentary.c
+++ src/documentary.c
@@ -2,6 +2,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+struct comment {
+    int type; // 0 - single comment , 1 - multiline comment
+    char** comment_data;
+    char* code_temp_string;
+};
+
 char* del_single_comment(char* str)
 {
     // may be on '//' symbols writing ' '
@@ -31,8 +38,34 @@ int del_multiline_comment_stars()
     // must be del only first star on string
     return 0;
 }
-int docs_gen()
+
+int docs_gen(char** array)
 {
-    // accepts array structs with contain comments and code blocks
-    return 0;
+    int count_of_lines;
+    const int MAX_STR_LEN = 1000;
+    count_of_lines = sizeof(array) / sizeof(array[0]);
+    struct comment* comments_array
+            = (struct comment*)malloc(count_of_lines * sizeof(struct comment));
+    char** temp_array = (char**)malloc(count_of_lines * sizeof(char*));
+    for (int i = 0; i < count_of_lines; i++) {
+        temp_array[i] = (char*)malloc(MAX_STR_LEN * sizeof(char));
+    }
+    int struct_line_number = 0;
+    for (int i = 0; i < count_of_lines; i++) {
+        if (single_comment_check(array[i]) == -1) { // switch for more cases
+            continue;
+        } else if (single_comment_check(array[i]) == 1) {
+            temp_array[struct_line_number] = del_single_comment(array[i]);
+            comments_array[struct_line_number].type = 0;
+            comments_array[struct_line_number].comment_data[0]
+                    = temp_array[struct_line_number];
+            if (single_comment_check(array[i + 1]) == 0) {
+                temp_array[struct_line_number]
+                        = del_single_comment(array[i + 1]);
+                comments_array[struct_line_number].code_temp_string
+                        = temp_array[struct_line_number];
+            }
+            struct_line_number++;
+        }
+    }
 }
