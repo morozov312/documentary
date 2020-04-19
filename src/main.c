5330574dd34d4469c8f73c700bd3c173fe51a9ba
Morozov312 <alexander.morozov312@gmail.com>
2020-04-20 00:46:52 +0700

    program termination in the absence of a code file

--- src/main.c
+++ src/main.c
@@ -2,11 +2,12 @@
 #include "handler.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 int main()
 {
     char path[] = "./path/path.txt";
-    const int max_len_inp_str = 1000;
-    const int max_quan_str = 10000;
+    const int max_len_inp_str = 500;
+    const int max_quan_str = 10;
     char** paths = (char**)malloc(max_quan_str * sizeof(char*));
     for (int i = 0; i < max_quan_str; i++) {
         paths[i] = (char*)malloc(max_len_inp_str * sizeof(char));
@@ -17,10 +18,18 @@ int main()
         }
     }
     paths = path_reading(path, paths);
-    int quan_of_paths = sizeof(paths) / sizeof(paths[0]);
+    int quan_of_paths = 0;
+    for (int i = 0; i < max_quan_str; i++) {
+        if (strlen(paths[i]) != 0) {
+            quan_of_paths++;
+        }
+    }
     char** file_data;
     for (int i = 0; i < quan_of_paths; i++) {
         file_data = document_handle(paths[i]);
+        if (file_data == 0) {
+            continue;
+        }
         docs_gen(file_data, paths[i]);
     }
     for (int i = 0; i < max_quan_str; i++) {
