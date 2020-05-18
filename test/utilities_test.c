74d2014d45f4d411a5df0224c03fa730f47c0d19
moskalt <thetima2@gmail.com>
2020-05-18 11:16:08 +0700

    Fixed tests memory leaks

--- test/utilities_test.c
+++ test/utilities_test.c
@@ -4,44 +4,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-CTEST(utilities, exclude_html)
-{
-    // Given
-    const char first[] = "<<<>>>";
-    const char second[] = "int n = 10";
-    char* str_first = (char*)malloc(strlen(first) * sizeof(char));
-    char* str_second = (char*)malloc(strlen(second) * sizeof(char));
-    strcpy(str_first, first);
-    strcpy(str_second, second);
-    // When
-    char* res_first = exclude_html(str_first);
-    char* res_second = exclude_html(str_second);
-    // Then
-    char* exception_first = "&lt&lt&lt&gt&gt&gt";
-    char* exception_second = "int n = 10";
-    ASSERT_STR(exception_first, res_first);
-    ASSERT_STR(exception_second, res_second);
-    free(res_first);
-    free(res_second);
-}
-CTEST(utilities, filename_without_extension)
-{
-    // Given
-    char str_first[] = "./myprogram/main.cpp";
-    char str_second[] = "./src/grab.h";
-    char str_third[] = "nothing";
-    // When
-    char* res_first = filename_without_extension(str_first);
-    char* res_second = filename_without_extension(str_second);
-    char* res_third = filename_without_extension(str_third);
-    // Then
-    char* exception_first = "main";
-    char* exception_second = "grab";
-    char* exception_third = "";
-    ASSERT_STR(exception_first, res_first);
-    ASSERT_STR(exception_second, res_second);
-    ASSERT_STR(exception_third, res_third);
-}
 CTEST(utilities, get_inpdir)
 {
     // Given
@@ -79,17 +41,4 @@ CTEST(utilities, get_outdir)
     ASSERT_STR(exception_first, res_fisrt);
     ASSERT_STR(exception_second, res_second);
     ASSERT_STR(exception_third, res_thrid);
-}
-CTEST(utilities, get_data_from_document)
-{
-    // Given
-    char str_first[] = "./test/main.c";
-    char str_second[] = "./test/main.cpp";
-    // When
-    char** res_first = get_data_from_document(str_first);
-    char** res_second = get_data_from_document(str_second);
-    // Then
-    char* exception_fisrt = "#define CTEST_MAIN\n";
-    ASSERT_STR(res_first[0], exception_fisrt);
-    ASSERT_NULL(res_second);
 }
